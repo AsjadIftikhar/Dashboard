4171bfb68f8140e0d2fdbc6cdc824ac1202e35d6
kai <iftikharasjad@gmail.com>
2022-01-09 02:40:52 +0500

    Base URL

--- test-api-main/hci/helper.py
+++ test-api-main/hci/helper.py
@@ -4,22 +4,24 @@ import requests
 
 
 class Data:
+    base_url = "http://127.0.0.1:8000"
+
     def get_instance_count(self):
-        r = requests.get('http://127.0.0.1:8000/apiViewSet/Instances/')
+        r = requests.get(self.base_url + '/apiViewSet/Instances/')
         return r.json()['count']
 
     def get_jobs_count(self):
-        r = requests.get('http://127.0.0.1:8000/apiViewSet/jobs/')
+        r = requests.get(self.base_url + '/apiViewSet/jobs/')
         return r.json()['count']
 
     def get_task_count(self):
-        r = requests.get('http://127.0.0.1:8000/apiViewSet/tasks/')
+        r = requests.get(self.base_url + '/apiViewSet/tasks/')
         return r.json()['count']
 
     def get_jobs(self):
-        r = requests.get('http://127.0.0.1:8000/apiViewSet/jobs/')
-        return r.json()['results']
-    def get_instances(self):
-        r = requests.get('http://127.0.0.1:8000/apiViewSet/Instances/')
+        r = requests.get(self.base_url + '/apiViewSet/jobs/')
         return r.json()['results']
 
+    def get_instances(self):
+        r = requests.get(self.base_url + '/apiViewSet/Instances/')
+        return r.json()['results']
